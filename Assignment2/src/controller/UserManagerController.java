5364dd245f4bd1cbffa3b7a37eaac32ab29a05dd
Ken Lau <klsl2008@hotmail.com>
2016-04-10 12:11:22 +0800

    added user manage controller for handling user managing related url

---
+++ Assignment2/src/controller/UserManagerController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javax.servlet.http.HttpServlet;
+
+public class UserManagerController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+}
