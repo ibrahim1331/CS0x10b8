6351c1a57ab4eef37aae96a0db7fc65da3f1eee5
Ken Lau <klsl2008@hotmail.com>
2016-04-22 22:39:06 +0800

    added java

--- Assignment2/src/service/SearchService.java
+++ Assignment2/src/service/SearchService.java
@@ -1,5 +1,22 @@
 package service;
 
-public class SearchService {
+import java.sql.Timestamp;
+import java.util.List;
 
+import dao.HotelDAO;
+import dao.HotelDAOImpl;
+import model.Search;
+import sqlwhere.core.Where;
+import sqlwhere.operators.compare.Like;
+import utils.Columns;
+
+public class SearchService {
+	HotelDAO hotelDAO = new HotelDAOImpl();
+	
+	public List<Search> search(String name, Timestamp fromDate, Timestamp toDate){
+		Where where = new Where(new Like(Columns.View.SearchView.HOTEL_NAME, "%"+name+"%"));
+		if(fromDate!=null){
+			where.and(new Equal(Columns.View.SearchView.))
+		}
+	}
 }
