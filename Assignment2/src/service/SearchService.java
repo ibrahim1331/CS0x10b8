49ad0383a1b4fe6dd2d54c8ce8afec9db3922793
Ken Lau <klsl2008@hotmail.com>
2016-04-22 23:26:06 +0800

    changed booking dao using new sql pstmt lib

--- Assignment2/src/service/SearchService.java
+++ Assignment2/src/service/SearchService.java
@@ -3,20 +3,31 @@ package service;
 import java.sql.Timestamp;
 import java.util.List;
 
+import dao.BookingDAO;
+import dao.BookingDAOImpl;
 import dao.HotelDAO;
 import dao.HotelDAOImpl;
-import model.Search;
+import model.Hotel;
 import sqlwhere.core.Where;
 import sqlwhere.operators.compare.Like;
 import utils.Columns;
 
 public class SearchService {
 	HotelDAO hotelDAO = new HotelDAOImpl();
+	BookingDAO bookingDAO = new BookingDAOImpl();
 	
-	public List<Search> search(String name, Timestamp fromDate, Timestamp toDate){
-		Where where = new Where(new Like(Columns.View.SearchView.HOTEL_NAME, "%"+name+"%"));
-		if(fromDate!=null){
-			where.and(new Equal(Columns.View.SearchView.))
+	public List<Hotel> searchHotels(String name, Timestamp fromDate, Timestamp toDate){
+		if(fromDate==null || toDate==null){
+			return this.searchHotel(name);
+		} else {
+			//get booking within date range first
+			
+			return null;
 		}
+	}
+	
+	public List<Hotel> searchHotel(String name){
+		Where where = new Where(new Like(Columns.Table.Hotel.NAME, "%"+name+"%"));
+		return hotelDAO.getHotels(where);
 	}
 }
