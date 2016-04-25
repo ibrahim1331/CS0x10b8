128e2d64a0fb866c3a89193bd70224b85742a751
Ken Lau <klsl2008@hotmail.com>
2016-04-25 21:55:56 +0800

    add temp booking

---
+++ Assignment2/src/model/TempBooking.java
@@ -0,0 +1,19 @@
+package model;
+
+public class TempBooking extends Booking {
+	private String roomNo;
+	private String hotelName;
+
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getRoomNo() {
+		return roomNo;
+	}
+	public void setRoomNo(String roomNo) {
+		this.roomNo = roomNo;
+	}
+}
